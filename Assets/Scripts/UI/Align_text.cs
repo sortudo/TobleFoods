5cb176bb0b2449cbfe29edff313da94fec9d423e
sortudo <eleazarf.braga@gmail.com>
2020-06-22 14:31:32 -0300

    MenuScene: Menu Scene is working
    - Menu Scene can adapts itself with differents portraits and resolutions
    - Added Audio Mixer
    - Play button now has click Sound
    - Play button has a new look
    - Text from buttons will align depending if there are pressed
    - A little tutorial explaining how the game works
    - MusicClass.cs: Control background music
    - Align_text.cs: Align text with button sprite
    - Change_scene.cs: Change to a chosen scene

---
+++ Assets/Scripts/UI/Align_text.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Align_text : MonoBehaviour
+{
+    public Text text;
+
+    public void setAligntoLower()
+    {
+        text.alignment = TextAnchor.LowerCenter;
+    }
+
+    public void setAligntoMiddle()
+    {
+        text.alignment = TextAnchor.MiddleCenter;
+    }
+}
---
+++ Assets/Scripts/UI/Change_scene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Change_scene : MonoBehaviour
+{
+    public Object Scene;
+
+    public void ChangetoScene()
+    {
+        SceneManager.LoadScene(sceneName: Scene.name);
+    }
+}
